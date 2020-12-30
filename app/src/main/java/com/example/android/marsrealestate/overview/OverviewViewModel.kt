586c10bf7eb06650097914a611f5dffdb20a09d2
Sujata Baral <sujata689@gmail.com>
2020-12-30 16:34:11 -0600

    add recyclerView to display images from network

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -39,9 +39,9 @@ class OverviewViewModel : ViewModel() {
     val status: LiveData<String>
         get() = _status
 
-    private val _property = MutableLiveData<MarsProperty>()
-    val property : LiveData<MarsProperty>
-        get() =_property
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+    val properties : LiveData<List<MarsProperty>>
+        get() =_properties
 
 
     private var viewModelJob = Job()
@@ -67,7 +67,7 @@ class OverviewViewModel : ViewModel() {
                 _status.value = "Success: ${listResult.size} Mars properties retrieved"
                 if(listResult.size > 0)
                 {
-                    _property.value = listResult[0]
+                    _properties.value = listResult
                 }
             }
             catch (ex :Exception)
